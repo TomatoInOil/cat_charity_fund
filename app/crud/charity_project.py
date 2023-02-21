97a8fea628ef3d09e910813380cd410e2d053520
Даниил Паутов <danpautoff@yandex.ru>
2023-02-21 17:25:59 +0300

    Добавление возможности сформировать отчёт в Google Sheets.

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,4 +1,29 @@
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import BaseCRUD
 from app.models import CharityProject
 
-charity_project_crud = BaseCRUD(model=CharityProject)
+
+class CharityProjectCRUD(BaseCRUD):
+    """Дополнение базовой реализации CRUD для модели проектов."""
+
+    async def get_projects_by_completion_rate(self, session: AsyncSession):
+        """Возвращает список закрытых проектов отсортированных по скорости сбора средств."""
+        stmt = (
+            select(
+                CharityProject,
+                (
+                    func.strftime("%s", CharityProject.close_date).__sub__(
+                        func.strftime("%s", CharityProject.create_date)
+                    )
+                ).label("completion_rate"),
+            )
+            .where(CharityProject.fully_invested.is_(True))
+            .order_by("completion_rate")
+        )
+        closed_projects = await session.execute(stmt)
+        return closed_projects
+
+
+charity_project_crud = CharityProjectCRUD(model=CharityProject)
---
+++ app/services/google_api.py
@@ -0,0 +1,98 @@
+import datetime
+from datetime import datetime, timedelta
+
+from aiogoogle import Aiogoogle
+
+from app.core.config import settings
+
+FORMAT = "%Y/%m/%d %H:%M:%S"
+REPORT_ROW_COUNT = 100
+REPORT_COLUMN_COUNT = 10
+SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/%s"
+
+
+async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
+    """
+    Создание чистого документа Google Sheets.
+    Возвращает id документа.
+    """
+    now_date_time = datetime.now().strftime(FORMAT)
+    service = await wrapper_services.discover("sheets", "v4")
+    spreadsheet_body = {
+        "properties": {
+            "title": f"QRKot. Отчет от {now_date_time}",
+            "locale": "ru_RU",
+        },
+        "sheets": [
+            {
+                "properties": {
+                    "sheetType": "GRID",
+                    "sheetId": 0,
+                    "title": "Лист1",
+                    "gridProperties": {
+                        "rowCount": REPORT_ROW_COUNT,
+                        "columnCount": REPORT_COLUMN_COUNT,
+                    },
+                }
+            }
+        ],
+    }
+    response = await wrapper_services.as_service_account(
+        service.spreadsheets.create(json=spreadsheet_body)
+    )
+    spreadsheet_id = response["spreadsheetId"]
+    return spreadsheet_id
+
+
+async def set_user_permissions(
+    spreadsheet_id: str, wrapper_services: Aiogoogle
+) -> None:
+    """Предоставление прав доступа администратору к документу."""
+    permissions_body = {
+        "type": "user",
+        "role": "writer",
+        "emailAddress": settings.admin_email,
+    }
+    drive_service = await wrapper_services.discover("drive", "v3")
+    await wrapper_services.as_service_account(
+        drive_service.permissions.create(
+            fileId=spreadsheet_id, json=permissions_body, fields="id"
+        )
+    )
+
+
+async def spreadsheets_update_value(
+    spreadsheet_id: str, closed_projects: list, wrapper_services: Aiogoogle
+) -> str:
+    """
+    Наполнение документа Google Sheets отчётными данными.
+    Возвращает ссылку на документ.
+    """
+    now_date_time = datetime.now().strftime(FORMAT)
+    sheets_service = await wrapper_services.discover("sheets", "v4")
+    table_values = [
+        ["Отчет от", now_date_time],
+        ["Топ проектов по скорости закрытия"],
+        ["Название проекта", "Время сбора", "Описание"],
+    ]
+    for project_and_completion_rate in closed_projects:
+        print(project_and_completion_rate)
+        project, completion_rate = project_and_completion_rate
+        completion_rate = timedelta(seconds=completion_rate)
+        new_row = [
+            project.name,
+            str(completion_rate),
+            project.description,
+        ]
+        table_values.append(new_row)
+
+    update_body = {"majorDimension": "ROWS", "values": table_values}
+    await wrapper_services.as_service_account(
+        sheets_service.spreadsheets.values.update(
+            spreadsheetId=spreadsheet_id,
+            range="A1:E30",
+            valueInputOption="USER_ENTERED",
+            json=update_body,
+        )
+    )
+    return SPREADSHEET_URL % spreadsheet_id
